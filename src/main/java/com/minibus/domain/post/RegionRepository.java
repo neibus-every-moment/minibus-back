b97fd2f7c7e3842495f64c210ea354a2220a25dd
Song Seung-hyun <thdtmdgus5505@gmail.com>
2022-01-18 23:57:42 +0900

    feat: create Region Entity, Repository
    Region
    Region Repository

---
+++ src/main/java/com/minibus/domain/post/RegionRepository.java
@@ -0,0 +1,9 @@
+package com.minibus.domain.post;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface RegionRepository extends JpaRepository<Region, Integer> {
+
+}
