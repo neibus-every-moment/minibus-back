00ce52ee6fec012da1cd37c99508628bb3891cd3
Song Seung-hyun <thdtmdgus5505@gmail.com>
2022-01-18 23:57:11 +0900

    feat: create Emoticon Entity, Repository
    Emoticon
    EmoticonRepository

---
+++ src/main/java/com/minibus/domain/post/EmoticonRepository.java
@@ -0,0 +1,6 @@
+package com.minibus.domain.post;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface EmoticonRepository extends JpaRepository<Emoticon, Integer> {
+}
