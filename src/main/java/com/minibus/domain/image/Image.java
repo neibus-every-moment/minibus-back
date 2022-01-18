5fd39c0f55ec11f0189386d52b1acfb8a2053b74
Song Seung-hyun <thdtmdgus5505@gmail.com>
2022-01-18 23:56:05 +0900

    feat: create Image Entity, Repository
    Image
    ImageRepository

---
+++ src/main/java/com/minibus/domain/image/Image.java
@@ -0,0 +1,29 @@
+package com.minibus.domain.image;
+
+import com.minibus.domain.BaseTimeEntity;
+import com.minibus.domain.post.Post;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity
+@Table(name = "IMAGE")
+public class Image extends BaseTimeEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "POST_ID")
+    private Post post;
+
+    private String path;
+}
