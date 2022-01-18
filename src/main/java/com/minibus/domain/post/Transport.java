681c38787d9f02e93f06576cad583a491f597faf
Song Seung-hyun <thdtmdgus5505@gmail.com>
2022-01-18 23:58:18 +0900

    feat: create Transport Entity, Repository
    Transport
    TransportRepository

---
+++ src/main/java/com/minibus/domain/post/Transport.java
@@ -0,0 +1,23 @@
+package com.minibus.domain.post;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity
+@Table(name = "TRANSPORT")
+public class Transport {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    private String name;
+}
---
+++ src/main/java/com/minibus/domain/post/TransportRepository.java
@@ -0,0 +1,6 @@
+package com.minibus.domain.post;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TransportRepository extends JpaRepository<Transport, Integer> {
+}
