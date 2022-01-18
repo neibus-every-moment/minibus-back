fe559f11661d598729b8378c8f9a65eabbb58a02
Song Seung-hyun <thdtmdgus5505@gmail.com>
2022-01-18 23:56:35 +0900

    feat: create ReportStatus
    create ReportStatus

---
+++ src/main/java/com/minibus/type/ReportStatus.java
@@ -0,0 +1,13 @@
+package com.minibus.type;
+
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+public enum ReportStatus {
+
+    BEFORE("처리 전"),
+    KEEP("보류"),
+    COMPLETE("처리완료");
+
+    private final String description;
+}
