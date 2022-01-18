152a5e854607a6b1e89c17546214ccf233ec3771
Song Seung-hyun <thdtmdgus5505@gmail.com>
2022-01-19 00:01:19 +0900

    feat: create ReportEtcDetail Entity, Repository
    ReportEtcDetail
    ReportEtcDetailRepository

---
+++ src/main/java/com/minibus/domain/report/ReportEtcDetail.java
@@ -0,0 +1,28 @@
+package com.minibus.domain.report;
+
+import com.minibus.domain.report.Report;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity
+@Table(name = "REPORT_ETC_DETAIL")
+public class ReportEtcDetail {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @OneToOne
+    @JoinColumn(name = "REPORT_ID")
+    private Report report;
+
+    private String content;
+}
