7e1554431b48bac43d37c7525c4a707bd1cea037
Song Seung-hyun <thdtmdgus5505@gmail.com>
2022-01-19 00:00:37 +0900

    feat: create ReportCategory Entity, Repository
    ReportCategory
    ReportCategoryRepository

---
+++ src/main/java/com/minibus/domain/report/ReportCategory.java
@@ -0,0 +1,23 @@
+package com.minibus.domain.report;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity
+@Table(name = "REPORT_CATEGORY")
+public class ReportCategory {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    private String content;
+}
