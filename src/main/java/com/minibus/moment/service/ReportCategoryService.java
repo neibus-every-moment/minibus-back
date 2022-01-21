d701d26f56d5ad00c6ba37aaa54a32ee78d14fe4
devjaein <devjaein@gmail.com>
2022-01-21 20:11:38 +0900

    feat: Create ReportCategoryService.java

--- src/main/java/com/minibus/moment/service/ReportCategoryService.java
+++ src/main/java/com/minibus/moment/service/ReportCategoryService.java
@@ -1,2 +1,25 @@
-package com.minibus.moment.service;public class ReportCategoryService {
+package com.minibus.moment.service;
+
+import com.minibus.moment.domain.report.ReportRepository;
+import com.minibus.moment.domain.reportCategory.ReportCategory;
+import com.minibus.moment.domain.reportCategory.ReportCategoryRepository;
+import com.minibus.moment.dto.ReportCategoryDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class ReportCategoryService {
+
+    private final ReportCategoryRepository reportCategoryRepository;
+
+    public ReportCategoryDto getReportCategoryList() {
+        List<ReportCategory> reportCategories = reportCategoryRepository.findAll();
+        return ReportCategoryDto.builder()
+                .data(reportCategories)
+                .build();
+    }
 }
