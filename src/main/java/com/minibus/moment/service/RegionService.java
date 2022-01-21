d81b6ecc0d55eb7b07f9db6c6505cb7f62434626
devjaein <devjaein@gmail.com>
2022-01-21 20:07:51 +0900

    feat: Create RegionService

--- src/main/java/com/minibus/moment/service/RegionService.java
+++ src/main/java/com/minibus/moment/service/RegionService.java
@@ -1,2 +1,24 @@
-package com.minibus.moment.service;public class RegionService {
+package com.minibus.moment.service;
+
+import com.minibus.moment.domain.region.Region;
+import com.minibus.moment.domain.region.RegionRepository;
+import com.minibus.moment.dto.RegionDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class RegionService {
+
+    private final RegionRepository regionRepository;
+
+    public RegionDto getRegionList() {
+        List<Region> regions = regionRepository.findAll();
+        return RegionDto.builder()
+                .data(regions)
+                .build();
+    }
 }
