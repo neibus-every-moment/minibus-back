25042fc6cc585d165df069525a46aa53b5160c83
Song Seung-hyun <thdtmdgus5505@gmail.com>
2022-01-23 14:57:49 +0900

    refactor: renamed

--- src/main/java/com/minibus/moment/domain/transport/TransportRepository.java
+++
@@ -1,6 +0,0 @@
-package com.minibus.moment.domain.transport;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface TransportRepository extends JpaRepository<Transport, Integer> {
-}
