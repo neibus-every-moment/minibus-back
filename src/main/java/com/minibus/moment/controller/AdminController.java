9a2444870ad0baf7b5c17b31da339cf2a2d66f78
Song Seung-hyun <thdtmdgus5505@gmail.com>
2022-01-24 22:14:35 +0900

    feat: implements blindPost, restorePost mapping
    - blindPost
    - restorePost

--- src/main/java/com/minibus/moment/controller/AdminController.java
+++ src/main/java/com/minibus/moment/controller/AdminController.java
@@ -1,6 +1,11 @@
 package com.minibus.moment.controller;
 
+import com.minibus.moment.dto.api.admin.BlindPost;
+import com.minibus.moment.dto.api.admin.RestorePost;
+import com.minibus.moment.service.BlindService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,5 +14,16 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class AdminController {
 
+    private final BlindService blindService;
+
+    @PostMapping("/blind")
+    public boolean blindPost(@RequestBody BlindPost.Request request){
+        return blindService.blindPost(request);
+    }
+
+    @PostMapping("/restore")
+    public boolean restorePost(@RequestBody RestorePost.Request request){
+        return blindService.restorePost(request);
+    }
 
 }
