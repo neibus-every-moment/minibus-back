8dc4db2b3a2bb88299bcc429129394868b6883a8
devjaein <devjaein@gmail.com>
2022-01-24 21:31:00 +0900

    feat: Make an exception

---
+++ src/main/java/com/minibus/moment/exception/TransportationNotFoundException.java
@@ -0,0 +1,7 @@
+package com.minibus.moment.exception;
+
+public class TransportationNotFoundException extends RuntimeException {
+    public TransportationNotFoundException(String s) {
+        super(s);
+    }
+}
--- src/main/java/com/minibus/moment/service/PostService.java
+++ src/main/java/com/minibus/moment/service/PostService.java
@@ -22,15 +22,21 @@ import com.minibus.moment.dto.ReportReasonDto;
 import com.minibus.moment.dto.api.CreatePost;
 import com.minibus.moment.dto.api.GetPostList;
 import com.minibus.moment.dto.api.ReportPost;
+import com.minibus.moment.exception.*;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.PageRequest;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 
 import javax.transaction.Transactional;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.util.Arrays;
 import java.util.List;
+import java.util.NoSuchElementException;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 import static com.minibus.moment.type.PostStatus.VISIBLE;
@@ -115,17 +121,16 @@ public class PostService {
 
     @Transactional
     public boolean like(Long postId) {
-        Post post = postRepository.findById(postId).orElseThrow(
-                // Todo 예외 처리
-        );
+        Post post = postRepository.findById(postId)
+                .orElseThrow(() -> new PostNotFoundException("해당 포스트를 찾지 못했습니다."));
         post.upLikeCount();
         return true;
     }
 
     @Transactional
     public boolean cancelLike(Long postId) {
-        Post post = postRepository.findById(postId).orElseThrow(
-                // Todo 예외 처리
+        Post post = postRepository.findById(postId)
+                .orElseThrow(() -> new PostNotFoundException("해당 포스트를 찾지 못했습니다.")
         );
         post.downLikeCount();
         return true;
@@ -133,11 +138,11 @@ public class PostService {
 
     @Transactional
     public boolean reportPost(ReportPost.Request request) {
-        ReportReason reportReason = reportReasonRepository.findById(request.getReportReasonId()).orElseThrow(
-                // Todo 예외 처리
+        ReportReason reportReason = reportReasonRepository.findById(request.getReportReasonId())
+                .orElseThrow(() -> new ReportReasonId("신고 사유가 존재하지 않습니다.")
         );
-        Post post = postRepository.findById(request.getId()).orElseThrow(
-                // Todo 예외 처리
+        Post post = postRepository.findById(request.getId())
+                .orElseThrow(() -> new PostNotFoundException("해당 포스트를 찾지 못했습니다.")
         );
 
         Report report = Report.builder()
@@ -160,9 +165,15 @@ public class PostService {
 
     @Transactional
     public Long createPost(CreatePost.Request request) {
-        Region region = regionRepository.findByNameEquals(request.getRegionName()).orElseThrow();
-        Transportation transportation = transportRepository.findByNameEquals(request.getTransportationName()).orElseThrow();
-        Emoticon emoticon = emoticonRepository.findByNameEquals(request.getEmoticonName()).orElseThrow();
+        Region region = regionRepository.findByNameEquals(request.getRegionName())
+                .orElseThrow(() -> new RegionNotFoundException("카테고리에서 지역을 선택해주세요.")
+        );
+        Transportation transportation = transportRepository.findByNameEquals(request.getTransportationName())
+                .orElseThrow(() -> new TransportationNotFoundException("카테고리에서 교통 수단을 선택해주세요.")
+        );
+        Emoticon emoticon = emoticonRepository.findByNameEquals(request.getEmoticonName())
+                .orElseThrow(() -> new EmoticonNotFoundException("카테고리에서 이모티콘을 선택해주세요.")
+        );
 
         Post post = Post.builder()
                 .content(request.getContent())
@@ -175,37 +186,38 @@ public class PostService {
         postRepository.save(post);
 
         // Todo 저장소에 실제 이미지를 저장하고 URL을 반환 하는 작업 구현 필요
-        String[] list = request.getBase64Image().split(" ");
-        String base64image = list[list.length - 1];
-        String fileName = LocalDate.now() + "_" + post.getId();
-        String imageUrl = ImageUploader.upload(base64image, fileName ,"png");
-        //
-        Image image = Image.builder()
-                .post(post)
-                .path(imageUrl)
-                .build();
-        imageRepository.save(image);
+//        String[] list = request.getBase64Image().split(" ");
+//        String base64image = list[list.length - 1];
+//        String fileName = LocalDate.now() + "_" + post.getId();
+//        String imageUrl = ImageUploader.upload(base64image, fileName ,"png");
+//        //
+//        Image image = Image.builder()
+//                .post(post)
+//                .path(imageUrl)
+//                .build();
+//        imageRepository.save(image);
 
         return post.getId();
     }
 
     public PostDto getPost(Long postId) {
-        Post post = postRepository.findById(postId).orElseThrow();
+        Post post = postRepository.findById(postId)
+                .orElseThrow(() -> new PostNotFoundException("해당 글을 조회할 수 없습니다."));
         return PostDto.from(post);
     }
 
     public List<Transportation> mapToTransportation(String string) {
         return Arrays.stream(string.split(",")).map(
-                t -> transportRepository.findByNameEquals(t).orElseThrow(
-                        // Todo 예외 처리
+                t -> transportRepository.findByNameEquals(t)
+                        .orElseThrow(() -> new TransportationNotFoundException("하나 이상의 교통수단을 선택해주세요.")
                 )
         ).collect(Collectors.toList());
     }
 
     public List<Region> mapToRegion(String string) {
         return Arrays.stream(string.split(",")).map(
-                r -> regionRepository.findByNameEquals(r).orElseThrow(
-                        // Todo 예외 처리
+                r -> regionRepository.findByNameEquals(r)
+                        .orElseThrow(() -> new RegionNotFoundException("하나 이상의 지역을 선택해주세요.")
                 )
         ).collect(Collectors.toList());
     }
