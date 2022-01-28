54933afc07d16aad39a150f8d9bdf9cb52a8511b
JisooPark <mmriann@gmail.com>
2022-01-28 19:13:06 +0900

    REFACTORING: Main Application
    S3 서버 정보가 담긴 aws.yml 과 application.yml 을 분리하기 위하여 수정

--- src/main/java/com/minibus/moment/MinibusBackApplication.java
+++ src/main/java/com/minibus/moment/MinibusBackApplication.java
@@ -1,15 +1,21 @@
 package com.minibus.moment;
 
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @EnableJpaAuditing
 @SpringBootApplication
 public class MinibusBackApplication {
 
+    public static final String APPLICATION_LOCATIONS = "spring.config.location="
+            + "classpath:application.yml,"
+            + "classpath:aws.yml";
+
     public static void main(String[] args) {
-        SpringApplication.run(MinibusBackApplication.class, args);
+        new SpringApplicationBuilder(MinibusBackApplication.class)
+                .properties(APPLICATION_LOCATIONS)
+                .run(args);
     }
 
 }
