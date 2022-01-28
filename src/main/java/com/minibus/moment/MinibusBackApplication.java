0f4c40605819ca3162b1f36eb6fa86a40a638bce
Song Seung-hyun <thdtmdgus5505@gmail.com>
2022-01-28 16:22:22 +0900

    fix: fix corsError
    - add corsConfigurer

--- src/main/java/com/minibus/moment/MinibusBackApplication.java
+++ src/main/java/com/minibus/moment/MinibusBackApplication.java
@@ -2,7 +2,10 @@ package com.minibus.moment;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @EnableJpaAuditing
 @SpringBootApplication
@@ -11,5 +14,16 @@ public class MinibusBackApplication {
     public static void main(String[] args) {
         SpringApplication.run(MinibusBackApplication.class, args);
     }
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**").allowedOrigins("http://localhost:3000");
+            }
+        };
+    }
+
 
 }
