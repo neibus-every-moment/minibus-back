4418d441c3fc2b4655857d359dddfc27520b9530
JisooPark <mmriann@gmail.com>
2022-02-07 17:05:00 +0900

    FEAT: Like Features

--- src/main/java/com/minibus/moment/domain/like/LikePostRepository.java
+++ src/main/java/com/minibus/moment/domain/like/LikePostRepository.java
@@ -1,6 +1,18 @@
 package com.minibus.moment.domain.like;
 
+import com.minibus.moment.domain.post.Post;
+import com.minibus.moment.domain.user.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
+@Repository
 public interface LikePostRepository extends JpaRepository<LikePost, Long> {
+
+    Long countAllByPost(Post post);
+
+    LikePost findAllByPostEqualsAndUserEquals(Post post, User user);
+
+    List<LikePost> findAllByUserEquals(User user);
 }
