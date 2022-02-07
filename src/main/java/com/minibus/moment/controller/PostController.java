d6b8e8f23b3c4d2559d3857ab89479881fd3f61e
JisooPark <mmriann@gmail.com>
2022-02-07 17:04:36 +0900

    FEAT: Like Feature
    1. likePost
     - 좋아요 기능. request 에 담긴 postId, userId로 좋아요 기능 실행 뒤 LikePostDto 반환
    2. likePostIdList
     -  request에서 userId 값을 LIKE_POST 에서 검색하여 List<Post> 로 반환
    3. getMyLikePostList
     - myRequest에서 userId 값을 검색하여 좋아요 한 글 정보 모두 반환

--- src/main/java/com/minibus/moment/controller/PostController.java
+++ src/main/java/com/minibus/moment/controller/PostController.java
@@ -1,10 +1,8 @@
 package com.minibus.moment.controller;
 
+import com.minibus.moment.dto.LikePostDto;
 import com.minibus.moment.dto.api.*;
-import com.minibus.moment.service.EmoticonService;
-import com.minibus.moment.service.PostService;
-import com.minibus.moment.service.RegionService;
-import com.minibus.moment.service.TransportationService;
+import com.minibus.moment.service.*;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
@@ -20,6 +18,7 @@ public class PostController {
     private final TransportationService transportationService;
     private final RegionService regionService;
     private final EmoticonService emoticonService;
+    private final LikePostService likePostService;
 
     @GetMapping("/transportation")
     public GetTransportationList.Response getTransportationList() {
@@ -35,7 +34,6 @@ public class PostController {
     public GetEmoticonList.Response getEmoticonList() {
         return new GetEmoticonList.Response(emoticonService.getEmoticonList());
     }
-
 
     @GetMapping("/reason")
     public GetReportReasonList.Response getReportReasonList() {
@@ -70,4 +68,21 @@ public class PostController {
         return new GetPost.Response(postService.getPost(postId));
     }
 
+    // 좋아요 기능. request 에 담긴 postId, userId로 좋아요 기능 실행 뒤 LikePostDto 반환
+    @PutMapping("/post/like")
+    public LikePostDto likePost(@RequestBody LikePostDto.Request request) throws Exception{
+        return likePostService.likePost(request.getPostId(), request.getUserId());
+    }
+
+    // request에서 userId 값을 LIKE_POST 에서 검색하여 List<Post> 로 반환
+    @PostMapping("/post/likePostIdList")
+    public List<Long> likePostIdList(@RequestBody LikePostDto.Request request) {
+        return likePostService.likePostList(request.getUserId());
+    }
+
+    // myRequest에서 userId 값을 검색하여 좋아요 한 글 정보 모두 반환
+    @PostMapping("/list/myLike")
+    public GetPostList.Response getMyLikePostList(@RequestBody GetPostList.MyRequest myRequest){
+        return new GetPostList.Response(likePostService.getMyLikePostList(myRequest));
+    }
 }
