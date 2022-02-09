19d9e8ab28bb5dd528393f276a652bbb511e304e
Song Seung-hyun <thdtmdgus5505@gmail.com>
2022-02-09 13:09:59 +0900

    feat: implements reportComment

---
+++ src/main/java/com/minibus/moment/dto/api/ReportComment.java
@@ -0,0 +1,17 @@
+package com.minibus.moment.dto.api;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+public class ReportComment {
+
+    @Getter
+    @NoArgsConstructor
+    @AllArgsConstructor
+    public static class Request {
+        private Long commentId; // 포스트 id
+        private String reportReason; // 신고 사유 id
+        private String detail; // Optional 신고 사유가 기타일 때 세부사항
+    }
+}
