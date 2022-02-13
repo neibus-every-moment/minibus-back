f51ce3b17c2daea977817d0146ee11285a8b0584
Song Seung-hyun <thdtmdgus5505@gmail.com>
2022-02-13 23:39:44 +0900

    config: configuration to add cors origin

--- src/main/java/com/minibus/moment/MinibusBackApplication.java
+++ src/main/java/com/minibus/moment/MinibusBackApplication.java
@@ -24,7 +24,7 @@ public class MinibusBackApplication {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:3000")
+                        .allowedOrigins("http://localhost:3000","https://minibus.netlify.app")
                         .allowedMethods(
                                 HttpMethod.GET.name(),
                                 HttpMethod.PATCH.name(),
